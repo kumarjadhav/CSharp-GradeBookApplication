8366805b6e2bee8d589d7611aa4ea786df3a24a3
Kumar Jadhav <M1069860@mindtree.com>
2022-08-16 17:12:51 +0530

    created nwe GradeBookTypes

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,14 +1,11 @@
-﻿using GradeBook.Enums;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿using System;
 namespace GradeBook.GradeBooks
 {
    public class StandardGradeBook : BaseGradeBook
     {
         public StandardGradeBook(string name) : base(name)
         {
+            Type = Enums.GradeBookType.Standard;
         }
     }
 }
